2b042e0d625054cbe1b5e631240c7f045c637c0a
Srikanth Reddy <duggempudi.srikanthreddy@gmail.com>
2019-10-13 09:48:05 +0530

    Initial commit

---
+++ login.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form method="POST" action="login1.php">
+
+Username: <input type="text" name="un"><br>
+Password: <input type="password" name="pwd"><br>
+<input type="submit" value="Login"><br>
+
+</form>
+
+</body>
+</html>
+
